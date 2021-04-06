096170591b7c2a7657d3d9cb0e8861b4ead64d18
Chantsova Anastasia <stasyadiv@gmail.com>
2021-04-07 01:11:20 +0300

    Imported modal.js
    Imported modal.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './sass/main.scss';
 import './js/mobile-menu.js';
 import './js/hero-link.js';
 import './js/slick.script';
-import './js/btn-up.js'
+import './js/btn-up.js';
+import './js/modal';
